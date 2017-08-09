a037690142604acdf6d80dcb6c044cf3ca49a965
Barbara Post <bpo@eonix.be>
2017-08-09 13:22:36 +0200

    Premier jet d'ajout d'une navbar bootstrap au projet, avec installation locale de bootstrap.

--- Barebone/ExtensionMetadata.cs
+++ Barebone/ExtensionMetadata.cs
@@ -8,8 +8,8 @@ namespace Barebone
         public IEnumerable<StyleSheet> StyleSheets => new StyleSheet[]
         {
             new StyleSheet("//fonts.googleapis.com/css?family=Open+Sans", 100),
-            new StyleSheet("//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", 200),
-            new StyleSheet("//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css", 210),
+            new StyleSheet("bower_components.bootstrap.dist.css.bootstrap.min.css", 200),
+            new StyleSheet("bower_components.bootstrap.dist.css.bootstrap-theme.min.css", 210),
             new StyleSheet("style.barebone.css",400)
         };
 
@@ -19,7 +19,7 @@ namespace Barebone
             new Script("//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js",200),
             new Script("//ajax.aspnetcdn.com/ajax/jquery.validation.unobtrusive/3.2.6/jquery.validate.unobtrusive.min.js",300),
             new Script("//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js",400),
-            new Script("//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",500),
+            new Script("bower_components.bootstrap.dist.js.bootstrap.min.js",500),
             new Script("script.barebone.js",600),
         };
 
