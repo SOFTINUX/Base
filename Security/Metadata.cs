5f35405406d52322ae47ce99c25038940be72bd4
Barbara Post <post.barbara@gmail.com>
2017-08-18 22:29:18 +0200

    oups

--- Security/Metadata.cs
+++ Security/Metadata.cs
@@ -3,13 +3,10 @@ using Infrastructure;
 
 namespace Security
 {
-    public class Metadata
-    {
         public class Metadata : IExtensionMetadata
         {
             public IEnumerable<StyleSheet> StyleSheets => new StyleSheet[] { };
             public IEnumerable<Script> Scripts => new Script[] { };
             public IEnumerable<MenuGroup> MenuGroups => new MenuGroup[] { };
         }
-    }
 }
