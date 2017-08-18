db63c7791be542dc70163d7e7f8acb900e30928c
Barbara Post <post.barbara@gmail.com>
2017-08-18 22:28:46 +0200

    Mise à jour en .net core 2.0.0 et création de l'extension "Security".

---
+++ Security/Metadata.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Infrastructure;
+
+namespace Security
+{
+    public class Metadata
+    {
+        public class Metadata : IExtensionMetadata
+        {
+            public IEnumerable<StyleSheet> StyleSheets => new StyleSheet[] { };
+            public IEnumerable<Script> Scripts => new Script[] { };
+            public IEnumerable<MenuGroup> MenuGroups => new MenuGroup[] { };
+        }
+    }
+}
