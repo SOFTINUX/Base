dedb2b854b66e87a7e2a743fcd558f6a009074fd
Barbara Post <bpo@eonix.be>
2017-08-22 12:57:02 +0200

    Correction chemins gulpfile.js, retrait des extensions en tant que dépendances de la webapp car avoir la même dll dans le bin de la webapp est Extensions de la webapp est la cause du menu dupliqué.

--- WebApplication/gulpfile.js
+++ WebApplication/gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require("gulp");
 gulp.task(
     "copy-extensions",
     function(cb) {
-        gulp.src(["../Extension1/bin/Debug/netstandard2.0/Extension1.dll"]).pipe(gulp.dest("Extensions"));
-        gulp.src(["../Extension2/bin/Debug/netstandard2.0/Extension2.dll"]).pipe(gulp.dest("Extensions"));
+        gulp.src(["../Extension1/bin/Debug/netcoreapp2.0/Extension1.dll"]).pipe(gulp.dest("Extensions"));
+        gulp.src(["../Extension2/bin/Debug/netcoreapp2.0/Extension2.dll"]).pipe(gulp.dest("Extensions"));
         cb();
     }
 );
