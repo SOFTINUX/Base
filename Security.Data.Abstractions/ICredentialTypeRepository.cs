7257126aafbe01cfbe78cfbc001f2e6bf72f5312
Barbara Post <bpo@eonix.be>
2017-09-05 13:30:07 +0200

    Définition des interfaces de repositories (WIP).

---
+++ Security.Data.Abstractions/ICredentialTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ExtCore.Data.Abstractions;
+using Security.Data.Entities;
+
+namespace Security.Data.Abstractions
+{
+    public interface ICredentialTypeRepository : IRepository
+    {
+        CredentialType WithKey(int entityId_);
+        IEnumerable<CredentialType> All();
+    }
+}
---
+++ Security.Data.Abstractions/IGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ExtCore.Data.Abstractions;
+using Security.Data.Entities;
+
+namespace Security.Data.Abstractions
+{
+    public interface IGroupRepository : IRepository
+    {
+        Group WithKey(int entityId_);
+        IEnumerable<Group> All();
+        void Create(Group entity_);
+        void Edit(Group entity_);
+        void Delete(int entityId_);
+    }
+}
