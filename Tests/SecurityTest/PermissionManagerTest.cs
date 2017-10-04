6d38dbb34e7168b9e35f187b5e43429e8a22400b
Barbara Post <barbara.post@live.be>
2017-10-04 17:53:04 +0200

    WIP permission manager test

--- Tests/SecurityTest/PermissionManagerTest.cs
+++ Tests/SecurityTest/PermissionManagerTest.cs
@@ -1,4 +1,9 @@
-﻿using SecurityTest.Util;
+﻿using Security;
+using Security.Data.Entities;
+using SecurityTest.Util;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using Xunit;
 
 namespace SecurityTest
@@ -6,10 +11,18 @@ namespace SecurityTest
     [Collection("Database collection")]
     public class PermissionManagerTest : BaseTest
     {
+        private const string CST_PERM_CODE = "test_perm";
+        private static string _assembly = typeof(PermissionManagerTest).Assembly.FullName;
+
         [Fact]
-        public void TestRoRw(DatabaseFixture fixture_)
+        public void TestGetFinalPermissionsRw(DatabaseFixture fixture_)
         {
             _fixture = fixture_;
+
+            Permission roPerm = new Permission { Code = CST_PERM_CODE, OriginExtension = _assembly };
+            Permission rwPerm = new Permission { Code = CST_PERM_CODE, OriginExtension = _assembly };
+            IEnumerable<Claim> claims = new PermissionManager().GetFinalPermissions(new List<Permission> { roPerm, rwPerm });
+            Assert.Equal(1, claims.Count());
 
             // TODO the test code
         }
