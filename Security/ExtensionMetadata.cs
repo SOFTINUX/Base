46d501bb6a83ead68cb216af5f056b581db63aaf
Barbara Post <post.barbara@gmail.com>
2017-10-27 21:54:41 +0200

    Fixed resources urls

--- Security/ExtensionMetadata.cs
+++ Security/ExtensionMetadata.cs
@@ -3,7 +3,7 @@ using Infrastructure;
 
 namespace Security
 {
-        public class Metadata : IExtensionMetadata
+        public class ExtensionMetadata : IExtensionMetadata
         {
             public IEnumerable<StyleSheet> StyleSheets => new StyleSheet[] { };
             public IEnumerable<Script> Scripts => new Script[] { };
