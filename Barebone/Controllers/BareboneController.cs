0e0fa454b2f6eb170b197a15ac043635e2130369
Xarkam <xarkam@gmail.com>
2017-11-04 21:32:47 +0100

    Add new json key, add access to configuration in barebone controller

--- Barebone/Controllers/BareboneController.cs
+++ Barebone/Controllers/BareboneController.cs
@@ -3,22 +3,29 @@ using Barebone.ViewModels.Barebone;
 using ExtCore.Data.Abstractions;
 using Infrastructure;
 using ControllerBase = Infrastructure.ControllerBase;
+using Microsoft.Extensions.Configuration;
 
 namespace Barebone.Controllers
 {
     public class BareboneController : ControllerBase
     {
         private readonly IDatabaseInitializer _databaseInitializer;
+        private readonly string corporateName, corporateLogo;
 
-        public BareboneController(IStorage storage_, IDatabaseInitializer databaseInitializer_) : base(storage_)
+        public BareboneController(IStorage storage_, IDatabaseInitializer databaseInitializer_, IConfiguration configuration_) : base(storage_)
         {
+            corporateName = configuration_["Corporate:Name"];
+            corporateLogo = configuration_["Corporate:BrandLogo"];
             _databaseInitializer = databaseInitializer_;
         }
 
         public ActionResult Index()
         {
+            ViewBag.CorporateName = corporateName;
+            ViewBag.CorporateLogo = corporateLogo;
             _databaseInitializer.CheckAndInitialize(this);
             return View(new IndexViewModelFactory().Create());
         }
+
     }
 }
