5aac2ad68dfa2620b7044623404ce3453074d5bd
Xarkam <xarkam@gmail.com>
2017-11-05 01:53:53 +0100

    WIP dashboard

--- Barebone/ExtensionMetadata.cs
+++ Barebone/ExtensionMetadata.cs
@@ -21,6 +21,7 @@ namespace Barebone
             new Script("/node_modules.jquery.dist.jquery.slim.min.js",100),
             new Script("/node_modules.popper.js.dist.umd.popper.min.js",200),
             new Script("/node_modules.bootstrap.dist.js.bootstrap.min.js",300),
+            new Script("/node_modules.bootstrap.dist.js.bootstrap.min.js.map",310),
             new Script("/node_modules.jquery_validation.dist.jquery.validate.min.js",400),
             new Script("/node_modules.jquery_validation_unobtrusive.jquery.validate.unobtrusive.js",500),
             new Script("/node_modules.js_cookie.src.js.cookie.js",600),
