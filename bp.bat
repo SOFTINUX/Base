51a085b9f64a64699f4ca2fb393944a8d693d0ec
Benjamin Nolmans <xarkam@gmail.com>
2017-11-08 20:23:48 +0100

    WIP: prepare remove gulp for copy extensions. Prepare script batch for build solution, clean, copy, ect...

---
+++ bp.bat
@@ -0,0 +1,64 @@
+@echo off
+
+IF "%1" == "/?" GOTO Help
+IF "%1" == "-h" GOTO Help
+IF "%1" == "clean" GOTO Clean
+IF "%1" == "build" GOTO Build
+IF "%1" == "copydeps" GOTO CopyDeps
+IF "%1" == "copyexts" GOTO CopyExts
+
+GOTO Clean
+
+:Clean
+echo ###################
+echo CLEANING SOLUTION
+echo ###################
+dotnet clean
+IF "%1" == "clean" GOTO End
+
+:Build
+echo ###################
+echo BUILD SOLUTION
+echo ###################
+dotnet build
+::gulp --gulpfile ./WebApplication/gulpfile.js copy-extensions
+IF "%1" == "build" GOTO End
+
+:CopyDeps
+::source https://stackoverflow.com/a/6258198
+echo ###################
+echo Copy Dependencies
+echo ###################
+::set src_folder=
+set dst_folder=.\WebApplication\bin\Debug\netcoreapp2.0\
+for /f "tokens=*" %%i in (dependencies.txt) DO (
+    ::xcopy /S/E "%src_folder%\%%i" "%dst_folder%"
+    xcopy /S/E/Y "%%i" "%dst_folder%"
+)
+IF "%1" == "copydeps" GOTO End
+
+:CopyExts
+echo ###################
+echo Copy extensions
+echo ###################
+set dst_folder=.\WebApplication\Extensions\
+for /f "tokens=*" %%i in (extentions.txt) DO (
+    xcopy /S/E/Y "%%i" "%dst_folder%"
+)
+
+:Help
+echo Avaliable parameters is :
+echo     - clean : clean solution
+echo     - build : only build solution
+echo     - copydeps : only copy dependencies (defined in dependecies.txt)
+echo     - copyexts : only copy extensions (defined in extentions.txt)
+echo.
+echo with no parameters, build proccess is:
+echo     - cleaning solution
+echo     - build solution
+echo     - copy dependecies
+echo     - copy extensions
+
+GOTO End
+
+:End
