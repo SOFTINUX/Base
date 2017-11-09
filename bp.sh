60e6873315edee7f1f66b88fa959d228f8311981
xarkam <xarkam@gmail.com>
2017-11-09 12:32:03 +0100

    Add script for linux/macos shell same as windows bat file

---
+++ bp.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+function clean
+{
+    echo "###################"
+    echo "CLEANING SOLUTION"
+    echo "###################"
+    dotnet clean
+    if [ "$1" = "clean" ]; then exit; fi
+}
+
+function build
+{
+    echo "###################"
+    echo "BUILD SOLUTION"
+    echo "###################"
+    dotnet build
+    if [ "$1" = "build" ]; then exit; fi
+}
+
+function copyexts
+{
+    echo "###################"
+    echo "Copy extensions"
+    echo "###################"
+    SET_DEST="./WebApplication/Extensions"
+    cat extensions.txt | sed 's/\\/\//g' | xargs -I % bash -c "cp % $SET_DEST; echo cp % $SET_DEST"
+    if [ "$1" = "copyexts" ]; then exit; fi
+}
+
+function copydeps
+{
+    echo "###################"
+    echo "Copy Dependencies"
+    echo "###################"
+    SET_DEST="./WebApplication/bin/Debug/netcoreapp2.0/"
+    cat dependencies.txt | sed 's/\\/\//g' | xargs -I % bash -c "cp % $SET_DEST; echo cp % $SET_DEST"
+    if [ "$1" = "copydeps" ]; then exit; fi
+}
+
+function help
+{
+    echo "Avaliable parameters is :"
+    echo "    - clean : clean solution"
+    echo "    - build : only build solution"
+    echo "    - copydeps : only copy dependencies (defined in dependecies.txt)"
+    echo "    - copyexts : only copy extensions (defined in extentions.txt)"
+    echo -ne "\n"
+    echo "with no parameters or unsupported parameters, build proccess is:"
+    echo "    - cleaning solution"
+    echo "    - build solution"
+    echo "    - copy dependencies"
+    echo "    - copy extensions"
+}
+
+
+case $1 in
+    clean)
+        clean
+        ;;
+    build)
+        build
+        ;;
+    copyexts)
+        copyexts
+        ;;
+    copydeps)
+        copydeps
+        ;;
+    -h|--help)
+        help
+        ;;
+    *)
+        clean
+        build
+        copydeps
+        copyexts
+        ;;
+esac
+#Clean
+echo "All done"
