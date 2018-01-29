ebb05b7c3aafd66583b447b3477e1087c248c65b
Benjamin Nolmans <xarkam@gmail.com>
2018-01-29 14:11:48 +0100

    List users

--- Security/Controllers/AdministrationController.cs
+++ Security/Controllers/AdministrationController.cs
@@ -1,4 +1,7 @@
-﻿using System.Net;
+﻿// Copyright © 2017 SOFTINUX. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for license information.
+
+using System.Net;
 using ExtCore.Data.Abstractions;
 using Infrastructure.Attributes;
 using Infrastructure.Enums;
---
+++ Security/Controllers/ListUsersController.cs
@@ -0,0 +1,43 @@
+﻿// Copyright © 2017 SOFTINUX. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ExtCore.Data.Abstractions;
+using Infrastructure.Attributes;
+using Microsoft.Extensions.Logging;
+using Security.Data.Entities;
+using ControllerBase = Infrastructure.ControllerBase;
+
+namespace Security.Controllers
+{
+    [Authorize("Admin", "Security")]
+    public class ListUsersController : ControllerBase
+    {
+        private ILogger _logger;
+        private readonly IStorage _storage;
+        private readonly UserManager<User> usersmanager_;
+        //public  List<IdentityUserRole<string>> Roles { get; set; }
+
+        public ListUsersController(IStorage storage_, UserManager<User> users_, ILoggerFactory loggerFactory_) : base(storage_, loggerFactory_)
+        {
+            _logger = _loggerFactory.CreateLogger(GetType().FullName);
+            _logger.LogInformation("oups");
+
+            usersmanager_ = users_;
+            _storage = storage_;
+        }
+
+        // GET
+        [Route("administration/listusers")]
+        public IActionResult Index()
+        {
+            ViewBag.userList = usersmanager_.Users.Select(u => new SelectListItem { Text = u.UserName, Value = u.Id }).ToList();
+            return View("ListUsers");
+        }
+    }
+}
