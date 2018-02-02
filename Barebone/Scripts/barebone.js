fe62bad22c428216333c1589a63d346e44dbe916
Barbara Post <post.barbara@gmail.com>
2018-02-02 22:51:25 +0100

    Fix #17

--- Barebone/Scripts/barebone.js
+++ Barebone/Scripts/barebone.js
@@ -31,3 +31,19 @@ $(document).ready(
         application.initializeEventHandlers();
     }
 );
+
+// AdminLTE menu tweak: remember which menu group was open
+// https://github.com/almasaeed2010/AdminLTE/issues/1806
+
+/** add active class and stay opened when selected */
+var url = window.location;
+
+// for sidebar menu entirely but not cover treeview
+// $('ul.sidebar-menu a').filter(function() {
+// 	 return this.href == url;
+// }).parent().addClass('active');
+
+// for treeview
+$('ul.treeview-menu a').filter(function() {
+	 return this.href == url;
+}).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
