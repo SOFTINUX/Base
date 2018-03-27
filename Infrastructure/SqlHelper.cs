69e090aac78570a0c8dc5f3eb71827baf1acef59
Barbara Post <post.barbara@gmail.com>
2018-03-27 11:03:18 +0200

    wip

---
+++ Infrastructure/SqlHelper.cs
@@ -0,0 +1,21 @@
+﻿using ExtCore.Data.Abstractions;
+
+namespace Infrastructure
+{
+    public class SqlHelper
+    {
+        private IStorageContext _context;
+
+        public SqlHelper(IStorageContext context_)
+        {
+            _context = context_;
+        }
+
+        public void ExecuteSqlFile(string path_)
+
+        {
+
+        }
+
+    }
+}
