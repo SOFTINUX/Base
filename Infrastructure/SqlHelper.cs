5bf98a66813eb5a921f45c0e48af86de0773cee8
Barbara Post <post.barbara@gmail.com>
2018-03-27 11:15:13 +0200

    Add SqlHelper

--- Infrastructure/SqlHelper.cs
+++ Infrastructure/SqlHelper.cs
@@ -1,4 +1,8 @@
-﻿using ExtCore.Data.Abstractions;
+﻿using System;
+using System.IO;
+using ExtCore.Data.Abstractions;
+using ExtCore.Data.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
@@ -11,10 +15,38 @@ namespace Infrastructure
             _context = context_;
         }
 
-        public void ExecuteSqlFile(string path_)
-
+        /// <summary>
+        /// Execute SQL code from an embedded resource SQL file.
+        /// </summary>
+        /// <param name="resourcePath_"></param>
+        /// <returns></returns>
+        public string ExecuteSqlResource(string resourcePath_)
         {
+            // TODO
+            return "not implemented";
+        }
 
+        /// <summary>
+        /// Execute SQL code from a plain SQL file.
+        /// </summary>
+        /// <param name="filePath_"></param>
+        /// <returns>Any error information, else null when no error happened</returns>
+        public string ExecuteSqlFile(string filePath_)
+        {
+            if (!File.Exists(filePath_))
+            {
+                return $"File {filePath_} not found";
+            }
+
+            try
+            {
+                ((StorageContextBase)_context).Database.ExecuteSqlCommand(File.ReadAllText(filePath_));
+            }
+            catch (Exception e)
+            {
+                return $"Error executing SQL: {e.Message} - {e.StackTrace}";
+            }
+            return null;
         }
 
     }
