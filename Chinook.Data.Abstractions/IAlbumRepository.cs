038dc01dfc5a0e68425f55fe5d7f7cb3f3b76a8f
Barbara Post <post.barbara@gmail.com>
2018-04-05 22:32:33 +0200

    Add unit test for Chinook

---
+++ Chinook.Data.Abstractions/IAlbumRepository.cs
@@ -0,0 +1,15 @@
+// Copyright © 2017 SOFTINUX. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Chinook.Data.Entities;
+using ExtCore.Data.Abstractions;
+
+namespace Chinook.Data.Abstractions
+{
+    public interface IAlbumRepository: IRepository
+    {
+        IEnumerable<Album> All();
+    }
+}
