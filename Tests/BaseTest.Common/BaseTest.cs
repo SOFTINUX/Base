d8dbaf299e29a2cfbd39fa8442d1846574021502
postb99 <post.barbara@gmail.com>
2018-06-02 23:22:54 +0200

    SOFB-44 - wip unit test

--- Tests/BaseTest.Common/BaseTest.cs
+++ Tests/BaseTest.Common/BaseTest.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright © 2017 SOFTINUX. All rights reserved.
 // Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 
+using ExtCore.Data.Abstractions;
+using Microsoft.AspNetCore.Identity;
 using Xunit.Abstractions;
 
 namespace BaseTest.Common
@@ -19,5 +21,18 @@ namespace BaseTest.Common
         /// Storage of an injected object that allows to write text to test output console.
         /// </summary>
         protected ITestOutputHelper _outputHandler;
+        /// <summary>
+        /// Storage of injected IStorage, if the derived class get some injected
+        /// </summary>
+        protected IStorage _storage;
+        /// <summary>
+        /// Storage of injected Identity UserManager, if the derived class get some injected
+        /// </summary>
+        protected UserManager<Security.Data.Entities.User> _userManager;
+
+        /// <summary>
+        /// Storage of injected Identity role manager, if the derived class get some injected
+        /// </summary>
+        protected RoleManager<IdentityRole<string>> _roleManager;
     }
 }
