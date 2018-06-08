6a92833b9b0156d5e6890e72dc404c9c8ad45a33
Xarkam <benjamin.nolmans@gmail.com>
2018-06-08 16:29:07 +0200

    Update to .NET Core 2.1

---
+++ Barebone/ToolBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barebone
+{
+    public static class ToolBox
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="s_"></param>
+        /// <returns></returns>
+        static string UppercaseFirst(string s_)
+        {
+            if (string.IsNullOrEmpty(s_))
+            {
+                return string.Empty;
+            }
+            char[] a = s_.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
