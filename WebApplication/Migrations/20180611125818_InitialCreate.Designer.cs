9e3916bce0dd2cecc9fd0e7b09528b02003e53da
Xarkam <xarkam@gmail.com>
2018-06-11 14:59:16 +0200

    Update Extcore to make migration possible

--- WebApplication/Migrations/20180611125818_InitialCreate.Designer.cs
+++ WebApplication/Migrations/20180611125818_InitialCreate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication;
 
 namespace WebApplication.Migrations
 {
     [DbContext(typeof(ApplicationStorageContext))]
-    partial class ApplicationStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20180611125818_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
