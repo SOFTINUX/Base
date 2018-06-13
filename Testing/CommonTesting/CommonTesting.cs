034da69ad7e19196f8a85338b4019962ba87dac3
Xarkam <benjamin.nolmans@gmail.com>
2018-06-13 14:00:37 +0200

    Add new test for use mock

---
+++ Testing/CommonTesting/CommonTesting.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CommonTesting
+{
+    public class CommonTesting
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Testing/SecurityTest/SecurityTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace SecurityTest
+{
+    public class SecurityTest
+    {
+        public SecurityTest()
+        {
+            throw new Exception("No initialisation in constructor. Please, make code");
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
