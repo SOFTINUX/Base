aaf991ea2335c1f60999fe3fdbfaf7f0ebc61274
Xarkam <benjamin.nolmans@gmail.com>
2018-06-13 14:13:08 +0200

    Add constructor into CommonTesting

--- Testing/CommonTesting/CommonTesting.cs
+++ Testing/CommonTesting/CommonTesting.cs
@@ -5,6 +5,11 @@ namespace CommonTesting
 {
     public class CommonTesting
     {
+        public CommonTesting()
+        {
+            throw new Exception("No initialisation in constructor. Please, make code");
+        }
+
         [Fact]
         public void Test1()
         {
