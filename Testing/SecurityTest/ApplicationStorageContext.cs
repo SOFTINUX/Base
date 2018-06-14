4746f2eedc4fe7882f703163eee14f2b6a773306
postb99 <post.barbara@gmail.com>
2018-06-14 22:04:37 +0200

    #SOFB-63 - add DbContext to test project

---
+++ Testing/SecurityTest/ApplicationStorageContext.cs
@@ -0,0 +1,42 @@
+﻿using ExtCore.Data.Abstractions;
+using ExtCore.Data.EntityFramework;
+using Infrastructure.Util;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Security.Data.Entities;
+
+namespace SecurityTest
+{
+    public partial class ApplicationStorageContext : IdentityDbContext<User, IdentityRole<string>, string>, IStorageContext
+    {
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<RolePermission> RolePermission { get; set; }
+        public DbSet<UserPermission> UserPermission { get; set; }
+
+        public ApplicationStorageContext(DbContextOptions<ApplicationStorageContext> options_)
+            : base(options_)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder_)
+        {
+            // Configure connection string
+            optionsBuilder_.UseSqlite("Data Source=basedb_tests.sqlite");
+#if DEBUG
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(new EfLoggerProvider());
+            base.OnConfiguring(optionsBuilder_.EnableSensitiveDataLogging().UseLoggerFactory(loggerFactory));
+#endif
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder_)
+        {
+            base.OnModelCreating(modelBuilder_);
+            this.RegisterEntities(modelBuilder_);
+        }
+
+    }
+}
+
