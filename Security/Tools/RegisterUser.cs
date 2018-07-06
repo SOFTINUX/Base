3f4990f5dd357d6c1c12e35b4a7079330d2838d8
Xarkam <benjamin.nolmans@gmail.com>
2018-07-06 12:06:02 +0200

    SOFB-72 WIP

---
+++ Security/Tools/RegisterUser.cs
@@ -0,0 +1,46 @@
+// Copyright © 2017 SOFTINUX. All rights reserved.
+// Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using ExtCore.Data.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Security.Data.Abstractions;
+using Security.Data.Entities;
+using Security.ViewModels.Account;
+using Security.ViewModels.Permissions;
+using Security.Data.EntityFramework;
+
+namespace Security.Tools
+{
+    public static class RegisterUser
+    {
+        /// <summary>
+        /// Check is user exist before create
+        /// </summary>
+        /// <returns>true if exist, false if not exist</returns>
+        public static bool IsUserExist(IStorage storage_, string validate)
+        {
+            IAspNetUsersRepository repo = storage_.GetRepository<IAspNetUsersRepository>();
+            return repo.FindByUserNameOrEmail(validate);
+        }
+
+        /// <summary>
+        /// Create user into application
+        /// </summary>
+        /// <param name="viewModel_">user object from view</param>
+        /// <returns>true if create, false if fail</returns>
+        public async static Task<bool> CreateNewUser(IStorage storage_,SignUpViewModel viewModel_)
+        {
+            if (IsUserExist(storage_, viewModel_.FirstName))
+            {
+                return true;
+            }
+            if (IsUserExist(storage_, viewModel_.Email))
+            {
+                return true;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
