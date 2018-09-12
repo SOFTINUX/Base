32722bf4b84f5821445efc927b564604613f340e
postb99 <post.barbara@gmail.com>
2018-09-12 22:00:36 +0200

    refact(security): arrangement and documentation of functions

--- Security/Controllers/GrantPermissionsController.cs
+++ Security/Controllers/GrantPermissionsController.cs
@@ -35,12 +35,14 @@ namespace Security.Controllers
             _roleManager = roleManager_;
         }
 
+        #region READ
+
         [PermissionRequirement(Permission.Admin)]
         [Route("administration/grantpermissions")]
         [HttpGet]
         public IActionResult Index()
         {
-           
+
             // Create a dictionary with all roles for injecting as json into grant permission page
             Dictionary<string, IdentityRole<string>> rolesList = new Dictionary<string, IdentityRole<string>>();
 
@@ -58,60 +60,6 @@ namespace Security.Controllers
 
             var model = ReadGrants.ReadAll(_roleManager, Storage, roleNameByRoleId);
             return View(model);
-        }
-
-        /// <summary>
-        /// Update a record indicating with which permission this role is linked to an extension.
-        /// </summary>
-        /// <param name="roleName_">Role name</param>
-        /// <param name="permissionId_">New permission level to save</param>
-        /// <param name="scope_">Scope</param>
-        /// <returns>JSON with "true" when it succeeded</returns>
-        [PermissionRequirement(Permission.Admin)]
-        [Route("administration/updaterolepermission")]
-        [HttpGet] // TODO change to POST
-        public async Task<IActionResult> UpdateRolePermission(string roleName_, string permissionId_, string scope_)
-        {
-            string roleId = (await _roleManager.FindByNameAsync(roleName_)).Id;
-            IRolePermissionRepository repo = Storage.GetRepository<IRolePermissionRepository>();
-            repo.Delete(roleId, scope_);
-            if (!string.IsNullOrEmpty(permissionId_.ToLowerInvariant()))
-                repo.Create(new RolePermission { RoleId = roleId, PermissionId = permissionId_.UppercaseFirst(), Scope = scope_ });
-            Storage.Save();
-            return new JsonResult(true);
-        }
-
-        /// <summary>
-        /// Delete the record indicating that a role is linked to an extension.
-        /// </summary>
-        /// <param name="roleName_"></param>
-        /// <param name="scope_"></param>
-        /// <returns></returns>
-        [HttpPost]
-        public async Task<IActionResult> DeleteRoleExtensionLink(string roleName_, string scope_)
-        {
-            string roleId = (await _roleManager.FindByNameAsync(roleName_)).Id;
-            IRolePermissionRepository repo = Storage.GetRepository<IRolePermissionRepository>();
-            if (repo.FindBy(roleId, scope_) != null)
-            {
-                repo.Delete(roleId, scope_);
-                Storage.Save();
-                return new JsonResult(true);
-            }
-            return new JsonResult(false);
-        }
-
-        /// <summary>
-        /// Create a role. Then create a set of records indicating to which extensions with which permission this role is linked to.
-        /// </summary>
-        /// <param name="model_"></param>
-        /// <returns></returns>
-        [Route("administration/savenewrole")]
-        [HttpPost]
-        public async Task<ObjectResult> SaveNewRoleAndItsPermissions(SaveNewRoleAndGrantsViewModel model_)
-        {
-            string error = await CreateRoleAndGrants.CheckAndSaveNewRoleAndGrants(model_, _roleManager, Storage);
-            return StatusCode(string.IsNullOrEmpty(error) ? 201 : 400, error);
         }
 
         /// <summary>
@@ -135,6 +83,48 @@ namespace Security.Controllers
             return StatusCode(200, Json(_role));
         }
 
+        #endregion
+
+        #region CREATE
+
+        /// <summary>
+        /// Create a role. Then create a set of records indicating to which extensions with which permission this role is linked to.
+        /// </summary>
+        /// <param name="model_"></param>
+        /// <returns></returns>
+        [Route("administration/savenewrole")]
+        [HttpPost]
+        public async Task<ObjectResult> SaveNewRoleAndItsPermissions(SaveNewRoleAndGrantsViewModel model_)
+        {
+            string error = await CreateRoleAndGrants.CheckAndSaveNewRoleAndGrants(model_, _roleManager, Storage);
+            return StatusCode(string.IsNullOrEmpty(error) ? 201 : 400, error);
+        }
+
+        #endregion
+
+        #region UPDATE
+
+        /// <summary>
+        /// Update a record indicating with which permission this role is linked to an extension.
+        /// </summary>
+        /// <param name="roleName_">Role name</param>
+        /// <param name="permissionId_">New permission level to save</param>
+        /// <param name="scope_">Scope</param>
+        /// <returns>JSON with "true" when it succeeded</returns>
+        [PermissionRequirement(Permission.Admin)]
+        [Route("administration/updaterolepermission")]
+        [HttpGet] // TODO change to POST
+        public async Task<IActionResult> UpdateRolePermission(string roleName_, string permissionId_, string scope_)
+        {
+            string roleId = (await _roleManager.FindByNameAsync(roleName_)).Id;
+            IRolePermissionRepository repo = Storage.GetRepository<IRolePermissionRepository>();
+            repo.Delete(roleId, scope_);
+            if (!string.IsNullOrEmpty(permissionId_.ToLowerInvariant()))
+                repo.Create(new RolePermission { RoleId = roleId, PermissionId = permissionId_.UppercaseFirst(), Scope = scope_ });
+            Storage.Save();
+            return new JsonResult(true);
+        }
+
         /// <summary>
         /// Update role name into database.
         /// </summary>
@@ -144,8 +134,36 @@ namespace Security.Controllers
         [Route("administration/updaterolename")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> UpdateRoleName(string name, string id){
+        public async Task<IActionResult> UpdateRoleName(string name, string id)
+        {
             return Json("Not yet implemented");
         }
+
+        #endregion
+
+        #region DELETE
+
+        /// <summary>
+        /// Delete the record indicating that a role is linked to an extension.
+        /// </summary>
+        /// <param name="roleName_"></param>
+        /// <param name="scope_"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> DeleteRoleExtensionLink(string roleName_, string scope_)
+        {
+            string roleId = (await _roleManager.FindByNameAsync(roleName_)).Id;
+            IRolePermissionRepository repo = Storage.GetRepository<IRolePermissionRepository>();
+            if (repo.FindBy(roleId, scope_) != null)
+            {
+                repo.Delete(roleId, scope_);
+                Storage.Save();
+                return new JsonResult(true);
+            }
+            return new JsonResult(false);
+        }
+
+        #endregion
+
     }
 }
--- Security/Scripts/security_user.js
+++ Security/Scripts/security_user.js
@@ -1,12 +1,15 @@
 // Copyright © 2017 SOFTINUX. All rights reserved.
 // Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 
+/* Anonymous function to avoid polluting global namespace */
 $(function () {
     window.toastr.options.positionClass = 'toast-top-right';
 
     browseForAvatar();
-    //freezePermissionCheckBox();
 
+    /* User interactions that trigger UI changes but no ajax call */
+
+    /* Click */
     $("#save_profile_btn").click(function (event) {
         edit_state("profile_form_fieldset", "save_profile_btn", event);
     });
@@ -15,24 +18,12 @@ $(function () {
         cancel_edit_state("profile_form", "profile_form_fieldset", "save_profile_btn", "Edit", event);
     });
 
-    $("#profile_form :input").bind("keyup change paste", function () {
-        input_changed("save_profile_btn");
-    });
-
     $("#change_pwd-btn").click(function (event) {
         edit_state("pwd_form_fliedset", "change_pwd-btn", event);
     });
 
     $("#cancel_change_pwd-btn").click(function (event) {
         cancel_edit_state("pwd_form", "pwd_form_fliedset", "change_pwd-btn", "Change", event);
-    });
-
-    $("#pwd_form :input").bind("keyup change paste", function () {
-        input_changed("change_pwd-btn");
-    });
-
-    $('#inputAvatar').change(function () {
-        $('#file_path').val($(this).val());
     });
 
     $('#add-role').click(function () {
@@ -53,47 +44,9 @@ $(function () {
             $("#edit-role-area").addClass("hidden");
     });
 
-    $('#save-add-role-btn').click(function () {
-        var roleNameInputElt = $("#role_name_input");
-         if (!roleNameInputElt.val()) {
-            window.toastr.warning('No role name given.', 'Role not saved!');
-            input_form_group_validator("#role_name_input");
-            return;
-        }
-        var selectedExtensions = [];
-        $("#selectedExtensions>option").each(function() {
-            selectedExtensions.push(this.value);
-        });
-        var postData = {
-            RoleName: roleNameInputElt.val(),
-            Extensions: selectedExtensions,
-            Permission: $("#newRolePermission").val()
-        };
-
-        // console.log(postData);
-
-        $.ajax("/administration/savenewrole", {method: 'POST', data: postData})
-        .done(function(data){
-            window.toastr.success(data, 'New role created');
-            input_form_group_set_error("#role_name_input", null);
-        })
-        .fail(function(jqXHR, testStatus){
-            var errMsg = jqXHR.responseText ? jqXHR.responseText : testStatus;
-            input_form_group_set_error("#role_name_input", errMsg);
-        });
-    });
-
     $('#cancel-add-role-btn').click(function () {
         $("#edit-role-area").addClass("hidden");
         $("#add-role-area").addClass("hidden");
-    });
-
-    $('#role_name_input').focusout(function () {
-        input_form_group_validator("#role_name_input");
-    });
-
-    $('#role_name_input').change(function () {
-        input_form_group_validator("#role_name_input");
     });
 
     $('#cancel-edit-role-btn').click(function () {
@@ -101,17 +54,7 @@ $(function () {
         $("#add-role-area").addClass("hidden");
     });
 
-    $('#save-edit-role-btn').click(function () {
-        if (!$("#name").val()) {
-            window.toastr.warning('No new role name given.', 'Role not updated!');
-            input_form_group_validator("#name");
-            return;
-        }
-
-        saveEditRole("edit-role-form");
-        //$("#edit-role-form").submit();
-    });
-
+    // permissions administration: collapsing
     $('#collapse').on('click', function () {
         var state = $(this).data('state');
         if (state === 'closed') {
@@ -144,6 +87,7 @@ $(function () {
         }
     });
 
+    // selected/unselected extensions management
     $('#btnRight').click(function (e) {
         var selectedOpts = $('#availableExtensions option:selected');
         if (selectedOpts.length === 0) {
@@ -156,6 +100,7 @@ $(function () {
         e.preventDefault();
     });
 
+    // selected/unselected extensions management
     $('#btnAllRight').click(function (e) {
         var selectedOpts = $('#availableExtensions option');
         if (selectedOpts.length === 0) {
@@ -168,6 +113,7 @@ $(function () {
         e.preventDefault();
     });
 
+    // selected/unselected extensions management
     $('#btnLeft').click(function (e) {
         var selectedOpts = $('#selectedExtensions option:selected');
         if (selectedOpts.length === 0) {
@@ -180,6 +126,7 @@ $(function () {
         e.preventDefault();
     });
 
+    // selected/unselected extensions management
     $('#btnAllLeft').click(function (e) {
         var selectedOpts = $('#selectedExtensions option');
         if (selectedOpts.length === 0) {
@@ -192,13 +139,86 @@ $(function () {
         e.preventDefault();
     });
 
+    // permission dropdown
     $('#acl-sel li').click(function (e) {
         var $target = $( e.currentTarget );
         $target.closest('.bs-dropdown-to-select-acl-group')
             .find('[data-bind="bs-drp-sel-acl-label"]').text( $(this).text() );
         $("input[name='acl-selected_value']" ).val( $(this).attr('data-value') );
     });
+
+    /* Keyup, change, paste */
+    $("#profile_form :input").bind("keyup change paste", function () {
+        input_changed("save_profile_btn");
+    });
+    
+    $("#pwd_form :input").bind("keyup change paste", function () {
+        input_changed("change_pwd-btn");
+    });
+
+    /* Change */
+    $('#inputAvatar').change(function () {
+        $('#file_path').val($(this).val());
+    });
+
+    $('#role_name_input').change(function () {
+        input_form_group_validator("#role_name_input");
+    });
+
+    /* Focusout */
+    $('#role_name_input').focusout(function () {
+        input_form_group_validator("#role_name_input");
+    });
+
+    /* User interactions that trigger ajax calls */
+
+    // save new role with its extensions and permission
+    $('#save-add-role-btn').click(function () {
+        var roleNameInputElt = $("#role_name_input");
+         if (!roleNameInputElt.val()) {
+            window.toastr.warning('No role name given.', 'Role not saved!');
+            input_form_group_validator("#role_name_input");
+            return;
+        }
+        var selectedExtensions = [];
+        $("#selectedExtensions>option").each(function() {
+            selectedExtensions.push(this.value);
+        });
+        var postData = {
+            RoleName: roleNameInputElt.val(),
+            Extensions: selectedExtensions,
+            Permission: $("#newRolePermission").val()
+        };
+
+        // console.log(postData);
+
+        $.ajax("/administration/savenewrole", {method: 'POST', data: postData})
+        .done(function(data){
+            window.toastr.success(data, 'New role created');
+            input_form_group_set_error("#role_name_input", null);
+        })
+        .fail(function(jqXHR, testStatus){
+            var errMsg = jqXHR.responseText ? jqXHR.responseText : testStatus;
+            input_form_group_set_error("#role_name_input", errMsg);
+        });
+    });
+
+    $('#save-edit-role-btn').click(function () {
+        if (!$("#name").val()) {
+            window.toastr.warning('No new role name given.', 'Role not updated!');
+            input_form_group_validator("#name");
+            return;
+        }
+
+        saveEditRole("edit-role-form");
+        //$("#edit-role-form").submit();
+    });
+
 });
+
+/* Function declared in global namespace
+so that they can be called by inline event handlers (onclick="...") 
+or by functions declared in anonymous function above */
 
 function browseForAvatar() {
     $('#file_browser').click(function (event) {
@@ -209,22 +229,12 @@ function browseForAvatar() {
 
 /* events handler */
 function listenToPermissionsCheckboxEvents() {
+    // TODO not referenced, check whether it is called
     console.log("attaching event handling");
     $('input').on('change', function (event) {
         console.log("input was changed!");
         event.preventDefault();
     });
-}
-
-function savePermission(scope, role, permission) {
-    // string roleName_, string permissionId_, string scope_
-    var params =
-    {
-        "roleName_": role,
-        "permissionId_": permission,
-        "scope_": scope
-    };
-    $.ajax("/administration/updaterolepermission", { data: params });
 }
 
 function edit_state(fieldsetid, editbtnid, event) {
@@ -261,39 +271,7 @@ function input_changed(editbtnid) {
     $("button#" + editbtnid).removeClass("hidden");
 }
 
-function checkClick() {
-    var splitted = $(event.target)[0].id.split('_');
-    var base = splitted[0] + "_" + splitted[1];
-    var activeCheckedPermissions = "NEVER";
-
-    if (event.target.checked) {
-        // when checking, impacted checkboxes become checked and disabled
-        switch (splitted[2]) {
-            case "ADMIN":
-                $("#" + base + "_WRITE, #" + base + "_READ").prop("checked", true).prop("disabled", true);
-                break;
-            case "WRITE":
-                $("#" + base + "_READ").prop("checked", true).prop("disabled", true);
-                break;
-        }
-        savePermission(splitted[0], splitted[1], splitted[2]);
-    }
-    else {
-        // when unchecking, first next checkbox becomes enabled
-        switch (splitted[2]) {
-            case "ADMIN":
-                $("#" + base + "_WRITE").prop("disabled", false);
-                activeCheckedPermissions = "WRITE";
-                break;
-            case "WRITE":
-                $("#" + base + "_READ").prop("disabled", false);
-                activeCheckedPermissions = "READ";
-                break;
-        }
-        savePermission(splitted[0], splitted[1], activeCheckedPermissions);
-    }
-}
-
+// display modal to choose what to remove about role
 function removeRoleLink() {
     console.log("Role name: ", $(event.target).parent().data("roleId"));
     var splitted = $(event.target).parent().data().roleId.split('_');
@@ -339,6 +317,9 @@ function input_form_group_set_error(el, errMsg) {
     }
 }
 
+/* Functions that trigger an ajax call */
+
+// Update the UI with selected role information. Ajax GET.
 function passSelectedRoleOnEdition(roleId_){
     $("#edit-role-group").removeClass("has-error");
     $.ajax("/administration/findrole", {data: {"roleId_": roleId_}}).done(function(data){
@@ -349,6 +330,53 @@ function passSelectedRoleOnEdition(roleId_){
     });
 }
 
+// Click in permission checkbox. Calls savePermission().
+function checkClick() {
+    var splitted = $(event.target)[0].id.split('_');
+    var base = splitted[0] + "_" + splitted[1];
+    var activeCheckedPermissions = "NEVER";
+
+    if (event.target.checked) {
+        // when checking, impacted checkboxes become checked and disabled
+        switch (splitted[2]) {
+            case "ADMIN":
+                $("#" + base + "_WRITE, #" + base + "_READ").prop("checked", true).prop("disabled", true);
+                break;
+            case "WRITE":
+                $("#" + base + "_READ").prop("checked", true).prop("disabled", true);
+                break;
+        }
+        savePermission(splitted[0], splitted[1], splitted[2]);
+    }
+    else {
+        // when unchecking, first next checkbox becomes enabled
+        switch (splitted[2]) {
+            case "ADMIN":
+                $("#" + base + "_WRITE").prop("disabled", false);
+                activeCheckedPermissions = "WRITE";
+                break;
+            case "WRITE":
+                $("#" + base + "_READ").prop("disabled", false);
+                activeCheckedPermissions = "READ";
+                break;
+        }
+        savePermission(splitted[0], splitted[1], activeCheckedPermissions);
+    }
+}
+
+// Ajax call to update data: role-permission link update. Ajax GET (TODO change to POST).
+function savePermission(scope, role, permission) {
+    // string roleName_, string permissionId_, string scope_
+    var params =
+    {
+        "roleName_": role,
+        "permissionId_": permission,
+        "scope_": scope
+    };
+    $.ajax("/administration/updaterolepermission", { data: params });
+}
+
+// Ajax call to update data: role with its related data update. Ajax POST.
 function saveEditRole(formId_){
     var $form =$("#"+formId_);
     var data = $form.serialize();
@@ -369,3 +397,6 @@ function saveEditRole(formId_){
         console.log(jqXHR, testStatus);
     });
 }
+
+
+
