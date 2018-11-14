b89eaed80b3860af0130d0f296be111ca284c48e
Xarkam <benjamin.nolmans@osames.org>
2018-11-14 13:56:04 +0100

    write first test.

--- src/Testing/Integration/src/Integration/spec.ts
+++ src/Testing/Integration/src/Integration/spec.ts
@@ -5,10 +5,35 @@
 
 describe('TypeScript', () => {
 
-    it('tests our site', () => {
+    it('Test Blank Sign In', () => {
+        cy.log('*** Get url site')
         cy.visit('http://localhost:5000/account/signin?ReturnUrl=%2F')  // change URL to match your dev URL
+        cy.log('*** Click signin button')
+        cy.get('.btn').click()
+        cy.log('*** Get username error class')
+        cy.get('#username').should('have.class', 'input-validation-error')
+        cy.log('*** Get password error class')
+        cy.get('#username').should('have.class', 'input-validation-error')
+    })
+
+    it('Test Sign In', () => {
+        cy.log('*** Get url site')
+        cy.visit('http://localhost:5000/account/signin?ReturnUrl=%2F')  // change URL to match your dev URL
+        cy.log('*** Fill username field')
         cy.get('#username').type('johndoe')
+        cy.log('*** Fill password field')
         cy.get('#password').type('123_Password{enter}')
+    })
+
+    it('Test Sign Out', () => {
+        cy.log('*** Click on user badge')
+        cy.get('.dropdown.user.user-menu').click()
+        cy.log('*** Click on Sign Out button')
+        cy.get('.dropdown.user.user-menu > ul:nth-child(2) > li:nth-child(2) > div:nth-child(2) > a')
+            .should('have.attr', 'href')
+            .and('include', '/Account/SignOut').then(() => {
+                cy.get('.dropdown.user.user-menu > ul:nth-child(2) > li:nth-child(2) > div:nth-child(2) > a').click()
+            })
     })
 
      // enable once we release updated TypeScript definitions
