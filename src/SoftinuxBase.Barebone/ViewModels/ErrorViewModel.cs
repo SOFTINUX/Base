04bd5e66bbb06e874eb63fbaedb58e0e4728317f
Xarkam <xarkam@gmail.com>
2019-01-09 10:53:52 +0100

    Convert bad files encoding from iso-8859-1 to utf-8

--- src/SoftinuxBase.Barebone/ViewModels/ErrorViewModel.cs
+++ src/SoftinuxBase.Barebone/ViewModels/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-// Copyright © 2017-2019 SOFTINUX. All rights reserved.
+// Copyright Â© 2017-2019 SOFTINUX. All rights reserved.
 // Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 
 namespace SoftinuxBase.Barebone.ViewModels
