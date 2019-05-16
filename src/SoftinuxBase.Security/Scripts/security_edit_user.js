0ead1594660957445cbec9c433e1c01898860a60
Xarkam <benjamin.nolmans@osames.org>
2019-05-16 14:39:49 +0200

    work in progress

---
+++ src/SoftinuxBase.Security/Scripts/security_edit_user.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+// Keyup, change, paste
+const profileForm = document.getElementById('profile_form').getElementsByTagName('input');
+profileForm.addEventListener('keyup', input_changed('save_profile_btn'));
+profileForm.addEventListener('paste', input_changed('save_profile_btn'));
+profileForm.addEventListener('change', input_changed('save_profile_btn'));
+
+document.getElementById('pwd_form').getElementsByTagName('input').addEventListener('keyup', input_changed('change_pwd-btn'));
+document.getElementById('pwd_form').getElementsByTagName('input').addEventListener('paste', input_changed('change_pwd-btn'));
+document.getElementById('pwd_form').getElementsByTagName('input').addEventListener('change', input_changed('change_pwd-btn'));
---
+++ src/SoftinuxBase.Security/Scripts/security_user_permissions.js
@@ -0,0 +1,114 @@
+﻿// Manage click on buttons
+    [].forEach.call(document.querySelectorAll('button'),
+        function (event_) {
+            event_.addEventListener('click',
+                function () {
+                    const addRoleArea = document.querySelector('#add-role-area');
+                    const editRoleArea = document.querySelector('#edit-role-area');
+
+                    switch (event_.id) {
+                        case 'save_profile_btn':
+                            edit_state('profile_form_fieldset', 'save_profile_btn', event_);
+                            break;
+                        case 'cancel_save_profile_btn':
+                            cancel_edit_state('profile_form', 'profile_form_fieldset', 'save_profile_btn', 'Edit', event_);
+                            break;
+                        case 'change_pwd-btn':
+                            edit_state('pwd_form_fliedset', 'change_pwd-btn', event_);
+                            break;
+                        case 'cancel_change_pwd-btn':
+                            cancel_edit_state('pwd_form', 'pwd_form_fliedset', 'change_pwd-btn', 'Change', event_);
+                            break;
+                        case 'add-role-btn':
+                            editRoleArea.style.display = 'none';
+                            addRoleArea.style.display = addRoleArea.style.display !== 'none' ? 'none' : 'block';
+                            break;
+                        case 'edit-role-btn':
+                            addRoleArea.style.display = 'none';
+                            editRoleArea.style.display = editRoleArea.style.display !== 'none' ? 'none' : 'block';
+                            break;
+                        case 'cancel-add-role-btn':
+                            editRoleArea.style.display = 'none';
+                            addRoleArea.style.display = 'none';
+                            break;
+                        case 'cancel-edit-role-btn':
+                            editRoleArea.style.display = 'none';
+                            addRoleArea.style.display = 'none';
+                            break;
+                        // Add selected/unselected extensions management
+                        case 'addRoleBtnRight':
+                            btnChevronMoveExtension(event_, '');
+                            break;
+                        case 'addRoleBtnAllRight':
+                            btnChevronMoveExtension(event_, '');
+                            break;
+                        case 'addRoleBtnLeft':
+                            btnChevronMoveExtension(event_, '');
+                            break;
+                        case 'addRoleBtnAllLeft':
+                            btnChevronMoveExtension(event_, '');
+                            break;
+                        // Edit selected/unselected extensions management
+                        case 'editRoleBtnRight':
+                            btnChevronMoveExtension(event_, 'to-right');
+                            break;
+                        case 'editRoleBtnAllRight':
+                            btnChevronMoveExtension(event_, 'to-right');
+                            break;
+                        case 'editRoleBtnLeft':
+                            btnChevronMoveExtension(event_, 'to-left');
+                            break;
+                        case 'editRoleBtnAllLeft':
+                            btnChevronMoveExtension(event_, 'to-left');
+                            break;
+                        default:
+                            break;
+                    }
+                });
+        });
+
+    document.getElementById('editRoleRightExtensionsList').addEventListener('click', event_ => {
+        row_clicked(event_.target.closest('div.row'));
+    }, false);
+
+    document.getElementById('editRoleLeftExtensionsList').addEventListener('click', event_ => {
+        row_clicked(event_.target.closest('div.row'));
+    }, false);
+
+    // permissions administration: collapsing
+    document.getElementById('collapse').addEventListener('click', () => {
+        const element = document.getElementById('collapse');
+        const state = document.getElementById('collapse').dataset.state;
+
+        var _subEl;
+        if (state === 'closed') {
+            element.dataset.state = 'open';
+            // TODO change icon to open double chevron
+
+            // open all the collapsed children
+            var _elements = Array.from(document.getElementsByClassName('extension-row collapsed'));
+            for (let item of _elements) {
+                item.classList.remove('collapsed');
+                item.setAttribute('aria-expanded', 'true');
+                _subEl = document.getElementsByClassName('row collapse');
+                for (var _i = 0, _ilen = _subEl.length; _i < _ilen; _i++) {
+                    _subEl[_i].classList.add('in');
+                }
+            }
+        } else {
+            element.dataset.state = 'closed';
+            // TODO change icon to closed double chevron
+            const elementRow = Array.from(document.getElementsByClassName('extension-row'));
+            // collapse all the children
+            for (let item of elementRow) {
+                if (!item.classList.contains('collapsed')) {
+                    item.classList.add('collapsed');
+                    item.setAttribute('aria-expanded', 'false');
+                    _subEl = document.getElementsByClassName('row collapse');
+                    for (var _j = 0, _jlen = _subEl.length; _j < _jlen; _j++) {
+                        _subEl[_j].classList.remove('in');
+                    }
+                }
+            }
+        }
+    }, false);
