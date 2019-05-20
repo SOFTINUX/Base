0cc83ea7501dd788bcff4c96faf56e85f35ddd26
Xarkam <benjamin.nolmans@osames.org>
2019-05-20 14:47:05 +0200

    Work in progress

--- src/SoftinuxBase.Security/Scripts/security_user.js
+++ src/SoftinuxBase.Security/Scripts/security_user.js
@@ -12,123 +12,6 @@ $(function () {
 
     browseForAvatar();
 
-    /* User interactions that trigger UI changes but no ajax call */
-
-    //// Manage click on buttons
-    //[].forEach.call(document.querySelectorAll('button'),
-    //    function (event_) {
-    //        event_.addEventListener('click',
-    //            function () {
-    //                const addRoleArea = document.querySelector('#add-role-area');
-    //                const editRoleArea = document.querySelector('#edit-role-area');
-
-    //                switch (event_.id) {
-    //                    case 'save_profile_btn':
-    //                        edit_state('profile_form_fieldset', 'save_profile_btn', event_);
-    //                        break;
-    //                    case 'cancel_save_profile_btn':
-    //                        cancel_edit_state('profile_form', 'profile_form_fieldset', 'save_profile_btn', 'Edit', event_);
-    //                        break;
-    //                    case 'change_pwd-btn':
-    //                        edit_state('pwd_form_fliedset', 'change_pwd-btn', event_);
-    //                        break;
-    //                    case 'cancel_change_pwd-btn':
-    //                        cancel_edit_state('pwd_form', 'pwd_form_fliedset', 'change_pwd-btn', 'Change', event_);
-    //                        break;
-    //                    case 'add-role-btn':
-    //                        editRoleArea.style.display = 'none';
-    //                        addRoleArea.style.display = addRoleArea.style.display !== 'none' ? 'none' : 'block';
-    //                        break;
-    //                    case 'edit-role-btn':
-    //                        addRoleArea.style.display = 'none';
-    //                        editRoleArea.style.display = editRoleArea.style.display !== 'none' ? 'none' : 'block';
-    //                        break;
-    //                    case 'cancel-add-role-btn':
-    //                        editRoleArea.style.display = 'none';
-    //                        addRoleArea.style.display = 'none';
-    //                        break;
-    //                    case 'cancel-edit-role-btn':
-    //                        editRoleArea.style.display = 'none';
-    //                        addRoleArea.style.display = 'none';
-    //                        break;
-    //                    // Add selected/unselected extensions management
-    //                    case 'addRoleBtnRight':
-    //                        btnChevronMoveExtension(event_, '');
-    //                        break;
-    //                    case 'addRoleBtnAllRight':
-    //                        btnChevronMoveExtension(event_, '');
-    //                        break;
-    //                    case 'addRoleBtnLeft':
-    //                        btnChevronMoveExtension(event_, '');
-    //                        break;
-    //                    case 'addRoleBtnAllLeft':
-    //                        btnChevronMoveExtension(event_, '');
-    //                        break;
-    //                    // Edit selected/unselected extensions management
-    //                    case 'editRoleBtnRight':
-    //                        btnChevronMoveExtension(event_, 'to-right');
-    //                        break;
-    //                    case 'editRoleBtnAllRight':
-    //                        btnChevronMoveExtension(event_, 'to-right');
-    //                        break;
-    //                    case 'editRoleBtnLeft':
-    //                        btnChevronMoveExtension(event_, 'to-left');
-    //                        break;
-    //                    case 'editRoleBtnAllLeft':
-    //                        btnChevronMoveExtension(event_, 'to-left');
-    //                        break;
-    //                    default:
-    //                        break;
-    //                }
-    //            });
-    //    });
-
-    //document.getElementById('editRoleRightExtensionsList').addEventListener('click', event_ => {
-    //    row_clicked(event_.target.closest('div.row'));
-    //}, false);
-
-    //document.getElementById('editRoleLeftExtensionsList').addEventListener('click', event_ => {
-    //    row_clicked(event_.target.closest('div.row'));
-    //}, false);
-
-    //// permissions administration: collapsing
-    //document.getElementById('collapse').addEventListener('click', () => {
-    //    const element = document.getElementById('collapse');
-    //    const state = document.getElementById('collapse').dataset.state;
-
-    //    var _subEl;
-    //    if (state === 'closed') {
-    //        element.dataset.state = 'open';
-    //        // TODO change icon to open double chevron
-
-    //        // open all the collapsed children
-    //        var _elements = Array.from(document.getElementsByClassName('extension-row collapsed'));
-    //        for (let item of _elements) {
-    //            item.classList.remove('collapsed');
-    //            item.setAttribute('aria-expanded', 'true');
-    //            _subEl = document.getElementsByClassName('row collapse');
-    //            for (var _i = 0, _ilen = _subEl.length; _i < _ilen; _i++) {
-    //                _subEl[_i].classList.add('in');
-    //            }
-    //        }
-    //    } else {
-    //        element.dataset.state = 'closed';
-    //        // TODO change icon to closed double chevron
-    //        const elementRow = Array.from(document.getElementsByClassName('extension-row'));
-    //        // collapse all the children
-    //        for (let item of elementRow) {
-    //            if (!item.classList.contains('collapsed')) {
-    //                item.classList.add('collapsed');
-    //                item.setAttribute('aria-expanded', 'false');
-    //                _subEl = document.getElementsByClassName('row collapse');
-    //                for (var _j = 0, _jlen = _subEl.length; _j < _jlen; _j++) {
-    //                    _subEl[_j].classList.remove('in');
-    //                }
-    //            }
-    //        }
-    //    }
-    //}, false);
-
     // permission dropdown
     $('#acl-sel li').click(function (event_) {
         const target = $(event_.currentTarget);
@@ -136,16 +19,6 @@ $(function () {
             .find('[data-bind="bs-drp-sel-acl-label"]').text($(this).text());
         $('input[name="acl-selected_value"]').val($(this).attr('data-value'));
     });
-
-    //// Keyup, change, paste
-    //const profileForm = document.getElementById('profile_form').getElementsByTagName('input');
-    //profileForm.addEventListener('keyup', input_changed('save_profile_btn'));
-    //profileForm.addEventListener('paste', input_changed('save_profile_btn'));
-    //profileForm.addEventListener('change', input_changed('save_profile_btn'));
-
-    //document.getElementById('pwd_form').getElementsByTagName('input').addEventListener('keyup', input_changed('change_pwd-btn'));
-    //document.getElementById('pwd_form').getElementsByTagName('input').addEventListener('paste', input_changed('change_pwd-btn'));
-    //document.getElementById('pwd_form').getElementsByTagName('input').addEventListener('change', input_changed('change_pwd-btn'));
 
     // Change
     $('#inputAvatar').change(function () {
@@ -350,14 +223,16 @@ function cancel_edit_state(formid_, fieldsetid_, editbtnid_, editbtntxt_, event_
 
 /**
  *
- * @param {any} editbtnid_ - editbtnid_
+ * @param {string} editbtnid_ - button html ID selector
  */
 function input_changed(editbtnid_) {
-    $(`#${editbtnid_}`).prop('disabled', false);
-    $(`#${editbtnid_}`).removeClass('btn-primary').addClass('btn-success');
-    $(`button#${editbtnid_}`).text('Save');
-    $(`#cancel_${editbtnid_}`).removeClass('hidden');
-    $(`button#${editbtnid_}`).removeClass('hidden');
+    const element = document.getElementById(editbtnid_);
+    element.disabled = false;
+    element.classList.remove('btn-primary');
+    element.classList.add('btn-success');
+    element.text('Save');
+    element.classList.remove('hidden');
+    document.getElementById(`#cancel_${element}`).classList.remove('hidden');
 }
 
 function row_clicked(event_) {
