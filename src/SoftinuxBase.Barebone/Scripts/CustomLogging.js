eb32e42c46ed9e602052da04aebacba656559f97
Xarkam <benjamin.nolmans@osames.org>
2019-09-19 16:23:50 +0200

    Add licence

--- src/SoftinuxBase.Barebone/Scripts/CustomLogging.js
+++ src/SoftinuxBase.Barebone/Scripts/CustomLogging.js
@@ -1,3 +1,5 @@
+// Copyright © 2017-2019 SOFTINUX. All rights reserved.
+// Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 class CustomLogging {
     constructor(title) {
         this.title = {
