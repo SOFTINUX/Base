1ebe7577a3a8c26e1565228e666bfa6478b956d0
Xarkam <benjamin.nolmans@osames.org>
2019-09-23 22:41:33 +0200

    Revert "Add licence"
    This reverts commit eb32e42c46ed9e602052da04aebacba656559f97.

--- src/SoftinuxBase.Barebone/Scripts/CustomLogging.js
+++ src/SoftinuxBase.Barebone/Scripts/CustomLogging.js
@@ -1,5 +1,3 @@
-// Copyright © 2017-2019 SOFTINUX. All rights reserved.
-// Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 class CustomLogging {
     constructor(title) {
         this.title = {
