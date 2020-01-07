e85a6fe022cac08ab78ea5a969ca43db22f69a00
postb99 <post.barbara@gmail.com>
2020-01-07 22:21:23 +0100

    Work on packing/unpacking permissions. Needs testing

--- src/SoftinuxBase.Security.PermissionParts/PermissionPackers.cs
+++ src/SoftinuxBase.Security.PermissionParts/PermissionPackers.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/ and 2017-2019 SOFTINUX
 // Licensed under MIT license. See License.txt in the project root for license information.
 
 using System;
@@ -9,26 +9,75 @@ namespace SoftinuxBase.Security.PermissionParts
 {
     public static class PermissionPackers
     {
+        // to be removed
         public static string PackPermissionsIntoString(this IEnumerable<Permissions> permissions)
         {
             return permissions.Aggregate("", (s, permission) => s + (char)permission);
         }
 
+        // to be removed
         public static IEnumerable<Permissions> UnpackPermissionsFromString(this string packedPermissions)
         {
             if (packedPermissions == null)
                 throw new ArgumentNullException(nameof(packedPermissions));
             foreach (var character in packedPermissions)
             {
-                yield return ((Permissions) character);
+                yield return ((Permissions)character);
             }
         }
 
-        public static Permissions? FindPermissionViaName(this string permissionName)
+        //public static Permissions? FindPermissionViaName(this string permissionName)
+        //{
+        //    return Enum.TryParse(permissionName, out Permissions permission)
+        //        ? (Permissions?)permission
+        //        : null;
+        //}
+
+        public static string PackPermissions(this IEnumerable<short> permissions_)
         {
-            return Enum.TryParse(permissionName, out Permissions permission)
-                ? (Permissions?) permission
-                : null;
+            return permissions_.Aggregate("", (s, permission) => s + (char)permission);
+        }
+
+        public static IEnumerable<short> UnpackPermissions(this string packedPermissions_)
+        {
+            if (packedPermissions_ == null)
+                throw new ArgumentNullException(nameof(packedPermissions_));
+            foreach (var character in packedPermissions_)
+            {
+                yield return ((short)character);
+            }
+        }
+
+        //TOTEST
+        /// <summary>
+        /// Pack permissions to compact format.
+        /// </summary>
+        /// <param name="dictionary_"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> PackPermissionsDictionary(this PermissionsDictionary dictionary_)
+        {
+            var packedDictionary = new Dictionary<string, string>();
+            foreach (var key in dictionary_.Dictionary.Keys)
+            {
+                packedDictionary.Add(key, dictionary_.Dictionary[key].PackPermissions());
+            }
+            return packedDictionary;
+        }
+
+        //TOTEST
+        /// <summary>
+        /// Unpack permissions to human readable format.
+        /// </summary>
+        /// <param name="dictionary_"></param>
+        /// <returns></returns>
+        public static PermissionsDictionary UnpackPermissionsDictionary(this Dictionary<string, string> dictionary_)
+        {
+            var unpackedDictionary = new PermissionsDictionary();
+            foreach (var values in dictionary_.Values)
+            {
+                unpackedDictionary.AddGrouped(values.UnpackPermissions());
+            }
+            return unpackedDictionary;
         }
 
     }
--- src/SoftinuxBase.Security.PermissionParts/PermissionsDictionary.cs
+++ src/SoftinuxBase.Security.PermissionParts/PermissionsDictionary.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 
 using System.Collections.Generic;
+using System.Linq;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SoftinuxBase.Security.PermissionPartsTests")]
 namespace SoftinuxBase.Security.PermissionParts
@@ -17,20 +18,39 @@ namespace SoftinuxBase.Security.PermissionParts
         /// Add a permission if it doesn't already exist.
         /// </summary>
         /// <param name="permission_"></param>
-        public void Add(Permissions permission_)
+        public void Add(short permission_)
         {
             var extensionName = GetExtensionName(permission_);
             Dictionary.TryGetValue(extensionName, out var permissions);
-            if(permissions == null)
+            if (permissions == null)
             {
                 permissions = new HashSet<short>();
                 Dictionary.Add(extensionName, permissions);
             }
-            permissions.Add((short)permission_);
-
+            permissions.Add(permission_);
         }
 
-        private string GetExtensionName(Permissions permission_)
+        //TOTEST
+        /// <summary>
+        /// Add a group of permissions. They should be defined in the same enum (faster method, less checks).
+        /// </summary>
+        /// <param name="permissions_"></param>
+        internal void AddGrouped(IEnumerable<short> permissions_)
+        {
+            var extensionName = GetExtensionName(permissions_.First());
+            Dictionary.TryGetValue(extensionName, out var permissions);
+            if (permissions == null)
+            {
+                permissions = new HashSet<short>();
+                Dictionary.Add(extensionName, permissions);
+            }
+            foreach(var permission in permissions_)
+            {
+                permissions.Add(permission);
+            }
+        }
+
+        private string GetExtensionName(short permission_)
         {
             return permission_.GetType().Assembly.GetName().Name;
         }
