2d871229f58413b2032db2159547490ef80a22c1
Xarkam <benjamin.nolmans@osames.org>
2020-08-14 21:24:35 +0200

    Add tests watchers script

---
+++ TestsWatchers/SecurityPermissionsTestsWatcher.bat
@@ -0,0 +1,1 @@
+dotnet watch --project ..\src\Testing\Unit\SoftinuxBase.Security.PermissionsTests\SoftinuxBase.Security.PermissionsTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
