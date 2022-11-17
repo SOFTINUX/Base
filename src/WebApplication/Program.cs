dde92e33e0488d9ef24310c2ec0aee002254fa98
Xarkam <xarkam@gmail.com>
2022-11-17 12:09:08 +0100

    Continue migration with https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples?view=aspnetcore-7.0

--- src/WebApplication/Program.cs
+++ src/WebApplication/Program.cs
@@ -1,34 +1,68 @@
 // Copyright © 2017-2022 SOFTINUX. All rights reserved.
 // Licensed under the MIT License, Version 2.0. See LICENSE file in the project root for license information.
 
+using System;
 using System.IO;
 using ExtCore.Data.Abstractions;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Serilog;
 using SoftinuxBase.Security.Data.EntityFramework;
+using SoftinuxBase.Security.ServiceConfiguration;
 using SoftinuxBase.WebApplication;
 
-var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
-ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
-IWebHostEnvironment hostingEnvironment_ = builder.Environment;
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ApplicationName = typeof(Program).Assembly.FullName,
+    ContentRootPath = Directory.GetCurrentDirectory(),
+    EnvironmentName = Environments.Staging,
 
-var extensionsPath = hostingEnvironment_.ContentRootPath + configuration["Extensions:Path"].Replace('/', Path.DirectorySeparatorChar).Replace('\\', Path.DirectorySeparatorChar);
+    // Look for static files in webroot
+    WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "wwwroot")
+});
+
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment hostingEnvironment = builder.Environment;
+IAntiforgery antiForgery = null;
+ILoggerFactory loggerFactory = null;
+
+var extensionsPath = hostingEnvironment.ContentRootPath + configuration["Extensions:Path"].Replace('/', Path.DirectorySeparatorChar).Replace('\\', Path.DirectorySeparatorChar);
+
+// Create the logger
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration, "Serilog")
+    .CreateBootstrapLogger();
 
 // Add services to the container.
+
+// Note: AddScoped : for services based on EF (once per request),
+// other values : AddTransient (stateless), AddSingleton (avoids to implement singleton pattern ourselves)
+builder.Services.AddSingleton(configuration);
+
+// SoftinuxBase/db context
 builder.Services.AddSoftinuxBase<ApplicationStorageContext>(builder.Configuration, extensionsPath);
+
+// Which database provider to use : Sqlite
 builder.Services.AddDbContext<ApplicationStorageContext>(options_ =>
 {
     options_.UseSqlite(builder.Configuration.GetConnectionString("Default"), sqliteOptions_ => sqliteOptions_.MigrationsAssembly(typeof(Program).Assembly.GetName().Name));
 });
-builder.Services.AddControllers();
+
+// Register database-specific storage context implementation.
 builder.Services.AddScoped<IStorageContext, ApplicationStorageContext>();
 
+builder.Services.AddControllers();
+
+// Register the Swagger generator, defining 1 or more Swagger documents
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Softinux Base API", Version = "v1" });
@@ -37,6 +71,13 @@ builder.Services.AddSwaggerGen(c =>
         c.IncludeXmlComments($"{extensionMetadata.CurrentAssemblyPath.Replace(@".dll", string.Empty)}.xml");
     }
 });
+
+builder.Services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().WriteTo.Seq("http://localhost:5341"));
+
+// Wait 30 seconds for graceful shutdown.
+builder.Host.ConfigureHostOptions(o => o.ShutdownTimeout = TimeSpan.FromSeconds(30));
 
 var app = builder.Build();
 
@@ -51,6 +92,6 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-//app.UseSoftinuxBase(hostingEnvironment_, loggerFactory_, configuration_, antiForgery_);
+app.UseSoftinuxBase(hostingEnvironment, loggerFactory, configuration, antiForgery);
 
 app.Run();
